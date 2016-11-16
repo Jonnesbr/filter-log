0614047f51fbe8246246d0967f6e49adc92f995d
João Paulo Oliveira <jpaulo.bcc@gmail.com>
2016-11-16 01:42:07 -0200

    Correção Clientes e Capturar Erros Ok

---
+++ application/views/admin/cliente/lista.php
@@ -0,0 +1,34 @@
+<?php header("Content-Type: text/html; charset=UTF-8", true); ?>
+<table id="table_perfil" class="table table-striped">
+    <thead>
+    <tr>
+        <th class="">Cliente</th>
+        <th class="">Endereço IP</th>
+        <th class="text-center"></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($result as $row) :?>
+        <tr>
+            <td><?php echo $row['nome']?></td>
+            <td><?php echo $row['ip']?></td>
+            <td class="pull-right">
+                <div class="btn-group">
+                    <a class="btn btn-primary" href="#"><i class="fa fa-bars fa-fw"></i> <?php echo $this->lang->line('acoes');?></a>
+                    <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
+                        <span class="fa fa-caret-down"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a href="<?php echo base_url().'Admin/Cliente/cadastro/'.$row['id'];?>" title="<?php echo $this->lang->line('editar');?>"><i class="fa fa-pencil fa-fw"></i><?php echo $this->lang->line('editar');?></a></li>
+                    </ul>
+                </div>
+            </td>
+        </tr>
+    <?php endforeach;?>
+    </tbody>
+</table>
+<div class="pagination-filter">
+    <?php if (isset($links) && !empty($links)){ ?>
+        <ul class="pagination"><?php echo $links; ?></ul>
+    <?php } ?>
+</div>
